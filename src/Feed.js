1542d0f5cbf42e1536fef24a872197a3ab0b38df
Karen Zoroglyan <karen.zoroglyan@mail.ru>
2022-06-01 21:03:01 +0400

    add Feed and InputOption

---
+++ src/Feed.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Feed.css";
+import CreateIcon from "@mui/icons-material/Create";
+import ImageIcon from "@mui/icons-material/Image";
+import InputOption from "./InputOption";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption title="Video" Icon={SubscriptionsIcon} color="#E7A33E" />
+          <InputOption title="Event" Icon={EventNoteIcon} color="#C0CBCD" />
+          <InputOption
+            title="Write article"
+            Icon={CalendarViewDayIcon}
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+
+    {/* Post */}
+
+    </div>
+  );
+}
+
+export default Feed;
